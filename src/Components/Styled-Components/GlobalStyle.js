ca4b2102a73d981a5caf649a9c6b4242fdbfba29
dci <wafi.amer.17@gmail.com>
2020-09-09 13:13:38 +0200

    first push to origin

---
+++ src/Components/Styled-Components/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-align: center;
+    background-color: rgb(190, 190, 190);
+  }
+`;
